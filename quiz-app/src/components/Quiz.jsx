51ac57ff55cb39797fa9a0c5b4f26d6cf15a5336
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-04-16 17:09:14 -0700

    quiz app again

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
+import QuizComplete from "../assets/quiz-complete.png";
 import QUESTIONS from '../resources/questions';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
+    // this is derived state
+    // a state created by using other state as vocal point
+    // better than creating lots of state and each uses useState
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = userAnswers.length === QUESTIONS.length;
 
     function handleSelectAnswer(answer) {
         setUserAnswers((prevUserAnswers) => {
@@ -13,12 +18,24 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return (
+            <div id = "summary">
+                <img src = {QuizComplete} alt = "Quiz complete" />
+                <h2>Quiz Completed</h2>
+            </div>
+        )
+    }
+
+    const shuffledActiveQuestionAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledActiveQuestionAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id = "quiz">
             <div id = "question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id = "answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer, ind) => {
+                    {shuffledActiveQuestionAnswers.map((answer, ind) => {
                         return (
                             <li key = {ind} className = "answer">
                                 <button onClick = {() => handleSelectAnswer(answer)}>{answer}</button>
